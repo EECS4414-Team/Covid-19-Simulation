133bc8af9c7667615fec96cd8e708a4bc3cb1cbf
Eli Frungorts <eli.frungorts@gmail.com>
2022-03-28 18:47:58 -0400

    graphing files

---
+++ analysis/grapher.py
@@ -0,0 +1,26 @@
+from glob import glob
+import simulation.global_state as global_state
+import matplotlib.pyplot as plt
+
+
+def build_graph(tuple_list):
+    healthy = [x[0] for x in tuple_list]
+    infected = [x[1] for x in tuple_list]
+    dead = [x[2] for x in tuple_list]
+    immune = [x[3] for x in tuple_list]
+
+    x_axis = list(range(global_state.cycle_number))
+
+    _, ax1 = plt.subplots()
+    line1, = ax1.plot(x_axis, healthy, color='red', label='healthy')
+    line2, = ax1.plot(x_axis, infected, color='blue', label='infected')
+    line3, = ax1.plot(x_axis, dead, color='black', label='dead')
+    line4, = ax1.plot(x_axis, immune, color='green', label='immune')
+    ax1.set_ylabel('count')
+    ax1.set_xlabel('cycle')
+    plt.xticks(list(range(1, global_state.cycle_number, global_state.cycle_number//10)))
+    plt.legend(handles=[line1,line2,line3,line4])
+    plt.title('count of states each person is in')
+    plt.savefig('graphs/out.png')
+    plt.clf()
+
